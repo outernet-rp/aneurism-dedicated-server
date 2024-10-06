2424e84dd537af2fb66d292483a5b159c6538b71
r0zemary <inhumanthree1@gmail.com>
2024-10-06 14:45:00 -0400

    Update server settings

--- scripts/entrypoint.sh
+++ scripts/entrypoint.sh
@@ -3,5 +3,5 @@ NOW=$( date '+%F_%H-%M-%S' )
 echo "[ANIV] Checking for updates..."
 ./aniv-ds.sh validate anonymous
 cd aniv-ds
-./aniv_server.x86_64 -map nightmare -maxplayers 24 -timestamps 2>&1 | tee ./logs/server_$NOW.log 
+./aniv_server.x86_64 -map nightmare -maxplayers 64 -timestamps 2>&1 | tee ./logs/server_$NOW.log 
 # ./aniv_server.x86_64 -map nightmare -timestamps
